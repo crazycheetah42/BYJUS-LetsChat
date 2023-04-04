7bbf761b538028c41689b50d73310a86c98a9dfb
Aryaman Sriram <76403763+crazycheetah42@users.noreply.github.com>
2023-04-04 13:49:59 -0400

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,3 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCbAIuulAfQyQUdnvvD-ysU0cOXV2maZik",
+    authDomain: "aryatech-techtweets.firebaseapp.com",
+    databaseURL: "https://aryatech-techtweets-default-rtdb.firebaseio.com",
+    projectId: "aryatech-techtweets",
+    storageBucket: "aryatech-techtweets.appspot.com",
+    messagingSenderId: "98575814660",
+    appId: "1:98575814660:web:102a8bdb875c3cd8162551",
+    measurementId: "G-D16242K29R"
+  };
+firebase.initializeApp(firebaseConfig);
+
 function addUser()
 {
     user_name = document.getElementById("user_name").value;
