90dde990e6ed8dd96b35a9b3198c1ab8d9130efd
Aryaman Sriram <76403763+crazycheetah42@users.noreply.github.com>
2023-04-04 13:50:32 -0400

    Update letschat_room.js

---
+++ letschat_room.js
@@ -0,0 +1,8 @@
+username = localStorage.getItem("user_name");
+console.log(username);
+username_paragraph = "Welcome, " + username + "!";
+document.getElementById("username_paragraph").innerHTML = username_paragraph;
+function addRoom()
+{
+    // adding soon
+}
